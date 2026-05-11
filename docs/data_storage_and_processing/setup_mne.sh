13d2848f5fb618f9dd430d75e0b810e7d20794cd
TommyClausner <tommy.clausner@proton.me>
2026-05-11 16:55:03 +0100

    scripts for setting up MNE, FLUX, jupyter

--- docs/data_storage_and_processing/setup_mne.sh
+++ docs/data_storage_and_processing/setup_mne.sh
@@ -7,8 +7,9 @@ JUPYTER_DIR="${ENV_DIR}/.jupyter"
 
 mkdir -p "${FLUX_DIR}"
 mkdir -p "${ENV_DIR}"
-mkdir -p "${MNE_ENV_DIR}"
 mkdir -p "${JUPYTER_DIR}"
+mkdir -p "${JUPYTER_DIR}/share"
+mkdir -p "${JUPYTER_DIR}/share/jupyter"
 
 cd "${FLUX_DIR}"
 
@@ -27,35 +28,37 @@ fi
 # add mamba to .bashrc
 if ! grep -qF "module load Mamba" ~/.bashrc 2>/dev/null; then
     cat >> ~/.bashrc <<EOF
-module load Mamba/23.11.0-0 2>/dev/null
-source /apps/system/easybuild/software/Mamba/23.11.0-0/etc/profile.d/mamba.sh
+module load Mamba/23.11.0-0
 EOF
 fi
 
 # add JUPYTER_DIR to .bashrc (later install the mne kernel on $DATA)
-if ! grep -qF "JUPYTER_PATH" ~/.bashrc 2>/dev/null; then
-  echo "export JUPYTER_PATH=\"${DATA}/jupyter\"" >> ~/.bashrc
-fi
+sed -i '/JUPYTER_PATH/d' ~/.bashrc
+echo "export JUPYTER_PATH=\"${JUPYTER_DIR}/share/jupyter\"" >> ~/.bashrc
+
+sed -i '/MNE_ENV_PATH/d' ~/.bashrc
+echo "export MNE_ENV_PATH=\"${MNE_ENV_DIR}\"" >> ~/.bashrc
+
+echo "starting interactive job for installation..."
 
 # send setup to interactive job
 srun \
---cpus-per-task=4 \
+--cpus-per-task=12 \
 --mem=16G \
---time=01:00:00 \
---job-name=setup_flux \
+--job-name=setup_mne \
+--time="01:00:00" \
 bash <<EOF
-source ~/.bashrc
+set -e
 
+rm -rf "${MNE_ENV_DIR}"
+source ~/.bashrc
 mamba create -y -p "${MNE_ENV_DIR}" python=3.11 mne jupyter jupyterlab mne-bids -c conda-forge
 
 echo "Installing ipykernel..."
-source activate "${MNE_ENV_DIR}"
-python -m ipykernel install \
+"${MNE_ENV_DIR}/bin/python" -m ipykernel install \
 --prefix="${JUPYTER_DIR}" \
 --name=mne \
 --display-name "Python (MNE)"
-
-mamba deactivate
 
 echo ""
 echo "Environment ready."
@@ -67,4 +70,6 @@ echo "    source activate ${MNE_ENV_DIR}"
 echo ""
 echo "Deactivate using:"
 echo "    conda deactivate"
+echo ""
+
 EOF
