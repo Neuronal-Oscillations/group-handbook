b9af8595d84cc24af4095bc34ce92148e541cf98
TommyClausner <tommy.clausner@proton.me>
2026-05-12 15:52:37 +0100

    streamline setup_mne.sh; rely on setup_sysenv.sh

--- code/hpc/setup_mne.sh
+++ code/hpc/setup_mne.sh
@@ -1,45 +1,11 @@
 #!/bin/bash
 
-ENV_DIR="${DATA}/environments"
-MNE_ENV_DIR="${ENV_DIR}/mne_env"
 FLUX_DIR="${DATA}/git-repos/FLUX"
-JUPYTER_DIR="${ENV_DIR}/jupyter"
 
 mkdir -p "${FLUX_DIR}"
-mkdir -p "${ENV_DIR}"
-mkdir -p "${JUPYTER_DIR}"
-mkdir -p "${JUPYTER_DIR}/share"
-mkdir -p "${JUPYTER_DIR}/share/jupyter"
-
 cd "${FLUX_DIR}"
 
 git clone https://github.com/FLUX-pipeline/Cerca
-
-# set up cache directory for conda / mamba
-if ! grep -qF "envs_dirs:" ~/.condarc 2>/dev/null; then
-    cat >> ~/.condarc <<EOF
-pkgs_dirs:
-  - $ENV_DIR/.conda/pkgs
-envs_dirs:
-  - $ENV_DIR/.conda/envs
-EOF
-fi
-
-# add mamba to .bashrc
-if ! grep -qF "module load Mamba" ~/.bashrc 2>/dev/null; then
-    cat >> ~/.bashrc <<EOF
-module load Mamba/23.11.0-0
-EOF
-fi
-
-# add JUPYTER_DIR to .bashrc (later install the mne kernel on $DATA)
-sed -i '/JUPYTER_PATH/d' ~/.bashrc
-echo "export JUPYTER_PATH=\"${JUPYTER_DIR}/share/jupyter\"" >> ~/.bashrc
-
-sed -i '/MNE_ENV_PATH/d' ~/.bashrc
-echo "export MNE_ENV_PATH=\"${MNE_ENV_DIR}\"" >> ~/.bashrc
-
-echo "starting interactive job for installation..."
 
 # send setup to interactive job
 srun \
@@ -56,7 +22,7 @@ mamba create -y -p "${MNE_ENV_DIR}" python=3.11 mne jupyter jupyterlab mne-bids 
 
 echo "Installing ipykernel..."
 "${MNE_ENV_DIR}/bin/python" -m ipykernel install \
---prefix="${JUPYTER_DIR}" \
+--prefix="${JUPYTER_PATH}/../.." \
 --name=mne \
 --display-name "Python (MNE)"
 
