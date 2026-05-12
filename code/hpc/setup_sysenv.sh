1cb36d84bfa2f52c5d905d54c96930bbee7e6710
TommyClausner <tommy.clausner@proton.me>
2026-05-12 15:10:12 +0100

    add conda setup to setup_sysenv.sh

--- code/hpc/setup_sysenv.sh
+++ code/hpc/setup_sysenv.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
+# setup path structure
 mkdir -p "${HOME}/.config"
 mkdir -p "${HOME}/.config/private"
 
 mkdir -p "${DATA}/environments"
+mkdir -p "${DATA}/environments/.conda"
+mkdir -p "${DATA}/environments/.conda/pkgs"
+mkdir -p "${DATA}/environments/.conda/envs"
+
 mkdir -p "${DATA}/environments/vscode"
 mkdir -p "${DATA}/environments/vscode/.vscode-extensions"
 mkdir -p "${DATA}/environments/vscode/.vscode-cli"
 mkdir -p "${DATA}/environments/vscode/.vscode-server"
+
 mkdir -p "${DATA}/environments/jupyter"
 mkdir -p "${DATA}/environments/jupyter/share"
 mkdir -p "${DATA}/environments/jupyter/share/jupyter"
 
+# setup ~/.bashrc
 lines=(
   "module load Mamba/23.11.0-0"
   "export PATH=\"${DATA}/git-repos/group-handbook/code/hpc/bin:\$PATH\""
@@ -29,6 +37,23 @@ done
 
 chmod +x "${DATA}/git-repos/group-handbook/code/hpc/bin/"*
 
+# setup ~/.condarc
+touch ~/.condarc
+if ! grep -q "${DATA}/environments/.conda/pkgs" ~/.condarc; then
+    cat >> ~/.condarc <<EOF
+pkgs_dirs:
+  - $DATA/environments/.conda/pkgs
+EOF
+fi
+
+if ! grep -q "${DATA}/environments/.conda/envs" ~/.condarc; then
+    cat >> ~/.condarc <<EOF
+envs_dirs:
+  - $DATA/environments/.conda/envs
+EOF
+fi
+
+# inform user about rfs_cred
 echo ""
 echo "---------------------------------------"
 echo "Store your RFS credentials in:"
